ad20bd59ab8e68fa2ad6c1344e6ccadef3899881
Andres Godinez <andres@barpres.com>
2020-02-10 18:09:07 -0600

    add controllers

---
+++ app/Http/Requests/UnitRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UnitRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $unit = $this->route('unit');
+
+        return [
+            'name' => [
+                Rule::unique('units', 'name')->ignore($unit)
+            ],
+            'ml' => 'required|numeric',
+            'sale_piece' => 'required',
+        ];
+    }
+}
